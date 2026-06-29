285445dd1765342f75da30b459913ed73c44dcae
Phey-Nyx <ph3ynyx.studio@gmail.com>
2026-06-29 11:15:04 -0400

    fix(assets): corriger les chemins du logo et du favicon sur GitHub Pages / fix logo and favicon paths on GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-const isGitHubActions = process.env.GITHUB_ACTIONS === "true";
-
 export default defineConfig({
   site: "https://ph3ynyxstudio-source.github.io",
-  base: isGitHubActions ? "/Ph3yNyx.Portfolio" : undefined,
+  base: "/Ph3yNyx.Portfolio/",
 });
